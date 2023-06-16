697ef24d901af2870547a12b90e58fbcaab83bed
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2023-06-16 14:57:51 +0200

    Add a blur+opacity effect to keep user focus on the selected menu

--- website/main.js
+++ website/main.js
@@ -1,17 +1,32 @@
-function focusNavigationOn(index) {
-    const navBar =  document.getElementsByTagName("nav")[0];
-    const navItems = navBar.children[0].children;
-    for (var i=0; i < navItems.length; i++) {
-        if (i !== index) {
-            navItems[i].classList.add("nav-item-masked");
+function updateNavigationMask(maskEnabled, index) {
+    const navBarElements =  document.getElementsByTagName("nav")[0].children[0];
+    const updateClass = (elt) => {
+        if (maskEnabled) {
+            elt.classList.add("masked");
+        } else {
+            elt.classList.remove("masked");
         }
     }
+    var elements = [...navBarElements.children];
+    if (maskEnabled) {
+        elements = elements.filter((elt, idx) => idx !== index);
+    }
+    elements.forEach((elt) => {
+        const svg = elt.getElementsByTagName("img");
+        if (svg.length > 0) {
+            updateClass(svg[0]);
+        } else {
+            updateClass(elt);
+        }
+    });
+    const main = document.getElementsByTagName("main")[0];
+    updateClass(main);
+}
+
+function focusNavigationOn(index) {
+    updateNavigationMask(true, index);
 }
 
 function unfocusNavigation() {
-    const navBar =  document.getElementsByTagName("nav")[0];
-    const navItems = navBar.children[0].children;
-    for (var i=0; i < navItems.length; i++) {
-        navItems[i].classList.remove("nav-item-masked");
-    }
+    updateNavigationMask(false);
 }
