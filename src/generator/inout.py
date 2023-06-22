ea22af24b8cd9e50799917e2d4609571467cc02c
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2023-06-22 19:32:25 +0200

    Consider assets as templates & double render pages in case content also contains vars

--- src/generator/inout.py
+++ src/generator/inout.py
@@ -5,6 +5,7 @@ Manages the input/output operations for the generator.
 # pylint: disable=import-error
 import os
 import shutil
+import jinja2 as jinja
 import projectfs as project_filesystem
 
 
@@ -38,15 +39,43 @@ def write_webpage(page_name, content):
         page_fd.write(content)
 
 
-def setup_dist_directory():
+def copy_asset_directory(dir_name : str):
+    target_dir = os.path.join(project_filesystem.get_dist_directory(), "assets", dir_name)
+    assets_dir = os.path.join(project_filesystem.get_assets_directory(), dir_name)
+    print(f"Copying {dir_name} directory to {target_dir}")
+    shutil.copytree(assets_dir, target_dir, dirs_exist_ok=True)
+
+
+def render_asset_directory(dir_name: str, base_url: str):
+    target_dir = os.path.join(project_filesystem.get_dist_directory(), "assets", dir_name)
+    asset_dir = os.path.join(project_filesystem.get_assets_directory(), dir_name)
+    if not os.path.isdir(target_dir):
+        os.mkdir(target_dir)
+    fs_loader = jinja.FileSystemLoader(asset_dir)
+    env = jinja.Environment(loader=fs_loader)
+    for filename in os.listdir(asset_dir):
+        template_location = os.path.join(target_dir, filename)
+        template = env.get_template(filename)
+        print(f"Rendering asset {template_location} in {target_dir}")
+        content = template.render(base=base_url)
+        with open(template_location, encoding="utf-8", mode="wt") as fd:
+            fd.write(content)
+
+
+
+def setup_dist_directory(base_url: str):
     """
     Create the distribution directory and copy all the public assets inside
     it.
+
+    base_url: The base URL of the website to generate
     """
     dist_dir = project_filesystem.get_dist_directory()
     assets_dir = project_filesystem.get_assets_directory()
     target_dir = os.path.join(dist_dir, "assets")
     if not os.path.isdir(dist_dir):
         os.mkdir(dist_dir)
-    print(f"Copying assets directory to {target_dir}")
-    shutil.copytree(assets_dir, target_dir, dirs_exist_ok=True)
+    copy_asset_directory("fonts")
+    copy_asset_directory("images")
+    render_asset_directory("styles", base_url)
+    render_asset_directory("scripts", base_url)
--- src/generator/main.py
+++ src/generator/main.py
@@ -7,14 +7,15 @@ import argparse
 import inout as io
 import pages
 import projectfs as project_filesystem
-from jinja2 import Environment, FileSystemLoader
+import jinja2 as jinja
 
 def render_page(base_url, style_path, content):
     templates_dir = project_filesystem.get_templates_directory()
-    fs_loader = FileSystemLoader(templates_dir)
-    template_env = Environment(loader=fs_loader)
+    fs_loader = jinja.FileSystemLoader(templates_dir)
+    template_env = jinja.Environment(loader=fs_loader)
     template = template_env.get_template("base.html")
-    return template.render(base=base_url, style=style_path, main=content)
+    base_render = template.render(base=base_url, style=style_path, main=content)
+    return template_env.from_string(base_render).render(base=base_url)
 
 
 def main(base_url):
@@ -23,7 +24,7 @@ def main(base_url):
 
     base_url: the base url used in all absolute links referencing the generated website.
     """
-    io.setup_dist_directory()
+    io.setup_dist_directory(base_url)
     for static_page in pages.STATIC_PAGES:
         content = io.read_html_content(static_page["input_content"])
         page = render_page(base_url, static_page["style"], content)
